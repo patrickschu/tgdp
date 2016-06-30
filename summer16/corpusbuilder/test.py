f2ed393887a7b8bef6f1d1919700dcda387dbe34
patrickschu <patrickschu@users.noreply.github.com>
2016-06-30 13:46:30 -0500

    Add files via upload

--- summer16/corpusbuilder/test.py
+++ summer16/corpusbuilder/test.py
@@ -1,57 +1,32 @@
-import json
-import argparse
+import pandas
 
-# import urllib
-# import os
-# import requests
+
+t={'assi':['1','2','3'],
+'name':['patrick','oberassi','unterassi']
+}
+
+
+t2={'assi':['1','2'],
+'name':['patrick','oberassi']
+}
+
+full=pandas.DataFrame(t)
+partial=pandas.DataFrame(t2)
+print full
+print partial
+
+res=pandas.merge(full, partial, how='inner', on='name')
+
+print "\n\nreulst"
+print  res
+
+
+#regex=9-134-2-2-a.wav
+#regex = "(\d+)-(\d+)-(\d+)-(\d+)-(\D+)\.wav"
+# thus 
+#[0]= iv_number
+#[1]= speaker_number
+#[2]= ??
+#[3]= gilbert_number
+#[4] = random alpha character			
 # 
-# # t=unicode("assi")
-# 
-# t='~/Desktop/downi'
-# directory=os.path.expanduser(t)
-# print directory
-# if not os.path.isdir(directory):
-# 	raise IOError("The outputfolder {} is not a valid directory.\nMake sure the folder exists in that place".format(directory))
-# 
-# 
-# link="http://www.kick.de/news/fussball/nationalslf/653241/artikel_storck_schwerer-kann-es-auch-nicht-werden.html"
-# link="http://www.kick.de/news/fussball/nationalslf/653241/artikel_storck_schwerer-kann-es-auch-nicht-werden.html"
-# resp = requests.head(link)
-# if resp.status_code != 200:
-# 	raise IOError("The link supplied ({}) does not seem valid (status is {}).\nPlease double-check!".format(link", "resp.status_code))
-
-
-# t={69: {u'current_residence': u'comfort'", "u'is_locked': 0", "u'gender': u'm'", "u'created_at': u'0000-00-00 00:00:00'", "u'updated_at': u'0000-00-00 00:00:00'", "u'childhood_residence': u'farm", "rural'", "u'island_id': 1", "u'DOB': 0", "u'informant_id': 70", "u'language_school': u'unknown'", "u'education_complete': u'unknown'", "u'questionnaire': u''", "u'language_home': u'unknown'}}
-# 
-# json.dump(t", "file('testttttttttt.txt'", "'w'))
-
-# tt=[1,2,3,2]
-# r=[i for i in tt if i == 10]
-# print r
-
-
-# 
-def main():
-    	parser = argparse.ArgumentParser()
-     	subparsers = parser.add_subparsers(help='sub-command help')
-      	settingsparser = subparsers.add_parser('settings') #i want a subparser called 'settings'
-      	settingsparser.add_argument('--fix_formatting', action='store_true') #this subparser shall have a --fix_formatting
-
-#Then I try to parse only the "settings" part like so:
-      	
-      	settings=parser.parse_args(['settings'])
-      	print settings
-      	
-#This seems to work. But then I add my other keys and things break: 
-
-    	keys=['alpha','beta','gamma','delta']
-    	for key in keys:
-    		parser.add_argument("--"+key, type=str, help="X")
-    	print parser
-    	args = parser.parse_args(['--alpha test'])
-    	print args
-	
-main()
-
-# #resi=["doss", "floresville", "universal city, 
-#                         "sisterdall", "columbus", "frelsburg/fayetteville",   "sisterdale", "kirby", "fredericksburg"", "wharton", "hamilton",  "solms", "giddings", "hamilton county", "garden ridge,  converse", "boerne", "8 mile creek", "comal co.", " giddings", "goliad co.", "wimberly", "olfem","pittsburgh", "pa", "ranch east of fredericksburg","bulverde", "freyburg/black jack", "mason", "cave creek","gill. co.", "columbus", "kirby", "castroville", "cave creek", "frelsburg", "stonewall", "wimberly", "texas", "doss", "dripping springs", "conroe", "warda", "wall", "cat spring", "clifton", "miles", "lee county", "lagrange", schulenburg", "new braunfels", "waldeck", "between wall & san angelo", "fisher", "hamilton county, luckenbach", "la grange", "freyburg", "converse", "gidings, mcqueeny", "spring branch", "san angelo", "houston, cherry springs", "geronimo", "new braunfels", "san marcos", "serbin", "winchester", "mcgregor", "comal county", "offen", "comfort", "san antonio", "fr", "marion", "mission valley", "midland", "freyburg/schulenburg, victoria", "seguin", "rio medina", "wall", "leroy, bracken", "warrenton", "round top", "cedar park", "freyburg", "comal co.]
--- summer16/gilberttools/gilbertfinder.py
+++ summer16/gilberttools/gilbertfinder.py
@@ -1,7 +1,6 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 ##getting relevant gilbert files
-header="\n\n---\n"
 
 import pandas
 import argparse
@@ -12,67 +11,43 @@ import os
 import shutil
 import Tkinter 
 import tkFileDialog
+import sys
+ 
+#tools
+header="\n\n---\n"
+
+#the operatordict matches functions to inputstrings
+operatordict={
+"<":operator.lt,
+"<=": operator.le,
+"==":operator.eq,
+"=":operator.eq,
+"!=":operator.ne,
+">=":operator.ge,
+">":operator.gt
+}
 
 
-##Note that some gilbert_numbers apply to several sentences. (all up to #20).
-def main(inputfile, column, search_term, move_data=True, output_csv=False):
-	parser = argparse.ArgumentParser()
 
-	print header, "Running the sentence finder", header
-	with open(inputfile, "r") as inputspread:
-		inputdata=pandas.read_csv(inputspread, encoding="utf-8")
-	for c in inputdata.columns:
-		inputname= c
-		inputtype= np.dtype(inputdata[c]).type
-		#print inputtype
-		parser.add_argument("--"+inputname, type=inputtype)	
-	
-	args= parser.parse_args(['--gilbert_number', '4', '--transcription', 'y'])
-	argsdict= vars(args)
-	print argsdict
-	#i hate that this comes in as a string as opposed to unicode, but seems nothing can be done about it
-	for item in [a for a in argsdict if argsdict[a]]:
-		if isinstance(argsdict[item], basestring):
-			print "Matching {} to '{}'".format(item, argsdict[item])
-			outputdata= inputdata[inputdata[item].str.contains(argsdict[item].decode("utf-8"))]
-		else:
-			print header, "Error: At this point, the sentencefinder does not accept numeric input like '{}'".format(argsdict[item]), header
-	print "{} sentences found".format(len(outputdata))
-	if output_csv:
-		outputdata.to_csv(output_csv, encoding="utf-8", index=False)
-		print header, "Written csv spreadsheet to {}".format(output_csv)
-	if move_data:
-		print header, "Copying data"
-		extracter(outputdata, 'gilbert_number')
-	
-			
-			
-			
-	
-
-	
-		
-
-#regex=9-134-2-2-a.wav
-#regex = "(\d+)-(\d+)-(\d+)-(\d+)-(\D+)\.wav"
-# thus 
-#[0]= iv_number
-#[1]= speaker_number
-#[2]= ??
-#[3]= gilbert_number
-#[4] = random alpha character			
-# 
-
-
+#helper functions
 def extracter(spreadsheet, column_name):
 	"""
-	extracter moves files
+	The extracter moves files.
+	Arguments input_folder and output_folder are set through GUI. 
+	Based on the values in the column called column_name in the spreadsheet, files are copied from input_folder to output_folder. 
+	Here, these are the gilbert_numbers in the spreadsheet fed from main(). 
+	The are matched to the file names. 
+	Each gilber_number gets its own directory in the output_folder. 
+	output_folder should be empty, at least not contain the same gilbert_numbers already.
+	Also copies all speaker files from input_folder to output_folder. 
 	"""
  	print header, "Running the extracter."
+ 	print "Please choose a directory that contains your sound files."
  	root=Tkinter.Tk()
 	root.withdraw()
 	root.update()
  	input_folder=tkFileDialog.askdirectory(title="Inputfolder: Please choose a directory that contains your corpus files")
+ 	"Please choose a directory that you want your files to be copied into."
  	root=Tkinter.Tk()
 	root.withdraw()
 	root.update()
@@ -80,59 +55,105 @@ def extracter(spreadsheet, column_name):
  	print header, "Copying files from '{}' to '{}'.".format(input_folder, output_folder)
  	#collecting input files
  	inputfiles=[]
- 	# for fili in os.walk(input_folder):
-# 		files=files+zip(fili[len(fili)-2],fili[len(fili)-1]) 
 	print "Locating files."	
 	for dirpath, subdirs, files in os.walk(input_folder):
 		for f in files:
 			inputfiles.append(os.path.join(dirpath, f))
-			if len(inputfiles) in [1000,2000,6000,12000,24000]:
+			if len(inputfiles) in [1000,2000,4000,8000,16000,24000]:
 				print "{} files processed, still working.".format(len(inputfiles))
-		
-
-	print "Found {} files".format(len(inputfiles))
-
+	print "Found {} files.".format(len(inputfiles))
  	#read from spreadsheet
  	# with open(spreadsheet, "r") as spreadsheet:
 #  		spreadsheet=pandas.read_csv(spreadsheet, encoding="utf-8")
  	numbers_to_be_extracted= spreadsheet[column_name].unique()
-
  	print header, "Gilbert numbers to be extracted:"
  	print ",".join([unicode(i) for i in numbers_to_be_extracted])
+	#copying speaker files
+	print header, "Copying speaker files."
+	speakerfiles=[f for f in inputfiles if re.match(".*\.txt", os.path.split(f)[1])]
+	os.mkdir(os.path.join(output_folder, "speakers"))
+	for s in speakerfiles:
+		shutil.copy2(s, os.path.join(output_folder, "speakers"))
  	#finding relevant input files
  	result=[]
 	for number in numbers_to_be_extracted:
-		print "Processing {}.".format(number)
+		print "Processing {}, creating folder '{}'.".format(number, number)
+		os.mkdir(os.path.join(output_folder, unicode(number)))
 		regex="(\d+)-(\d+)-(\d+)-"+number.astype('U')+"-(\D+)\.wav"
   		findings= [f for f in inputfiles if re.match(regex, os.path.split(f)[1])]
   		result= result+findings
   		for find in findings:
-	   		shutil.copy2(find, os.path.join(output_folder, os.path.split(find)[1]))
-  		
+  			shutil.copy2(find, os.path.join(output_folder, unicode(number), os.path.split(find)[1]))	
   	print header, "{} files have been copied to {}.".format(len(result), output_folder)
- 	
- 	
+
+
+###MAIN###
+
+##Note that some gilbert_numbers apply to several sentences. (all up to #20).
+def main():
+	"""
+	Matches gilbert_sentences as contained in the inputfile to user input. 
+	Input can be numeric (e.g. entries in inputfile[items] that are = 2).
+	Input can be characters (e.g. all entries in inputfile[transcription] that contain 'ɪ').
+	If output_csv is set, the resulting data will be written to a csv file. 
+	If move_data is set, the resulting data will be used to copy the relevant files to desired folder. 
+	 
+	"""
+	parser = argparse.ArgumentParser()
+	#reading in the spreadsheet
+	parser.add_argument("input_file", type=str, help="The input_file is the spreadsheet containing the Gilbert sentence numbers, target forms, etc.")
+	initial_args, extras=parser.parse_known_args()
+	print initial_args.input_file
+	print header, "Running the sentence finder", header
+	with open(initial_args.input_file, "r") as inputspread:
+		inputdata=pandas.read_csv(inputspread, encoding="utf-8")
 	
- 	# for n in numbers_to_be_extracted:
-#  	
-#  		print n
-# 		
-		# we can have one dir, thus listdir
-		# subdirs showing up, we need to list as well
-		# walk
-		#then match filenames to regex
-		#copy to outputdir
-		#9-134-2-2-a.wav
-		#number before a is gilbert_number
-		
-		
+	#building up criteria	
+	for c in inputdata.columns:
+		parser.add_argument("--"+c, type=lambda s: unicode(s, 'utf8'))
+	parser.add_argument("--move_data", action='store_true', help="If this flag is set, files that match criteria are copied from input_folder to output_folder. input_folder and output_folder are set through graphical interface.")
+	parser.add_argument("--output_csv", action='store_true', help="If set, this flag is set, a csv file containing the sentences that match the criteria is written. The output_folder is set through graphical interface.")
+	args= parser.parse_args()
+	argsdict= vars(args)
+	intinput=re.compile(ur"(["+"".join(operatordict.keys())+"]+)(?:\s*?)([0-9]+)", re.UNICODE)
+	strinput=re.compile(ur"(["+"".join(operatordict.keys())+"]+)(?:\s*?)(\D)", re.UNICODE)
+	outputdata=inputdata
 	
-#extracter('/Users/ps22344/Downloads/tgdp-master/summer16/files/gilbert_questions_withtrans_final.csv','gilbert_number')
+	#the actual matching happens here, two procedures for strings versus numbers
+	#in every iteration, we keep only the intersection btw the two dataframes
+	for item in [a for a in argsdict if argsdict[a] and a in inputdata.columns]:
+		print "Processing", type(argsdict[item])
+		if re.match(strinput, argsdict[item]):
+			#what if they try to use > or < with a string
+			matcher=re.findall(strinput, argsdict[item])[0]
+			print "Matching string", item, " ".join(matcher)
+			outputdata=pandas.merge(outputdata, inputdata[inputdata[item].str.contains(matcher[1])], how='inner')
+		elif re.match(intinput, argsdict[item]):
+			matcher=re.findall(intinput, argsdict[item])[0]
+			print "Matching number", item, " ".join(matcher)
+			outputdata=pandas.merge(outputdata, inputdata[operatordict[matcher[0]](inputdata[item], float(matcher[1]))], how='inner')
+		else:
+			print "\nError: No match for the input", item
+	print header, "Resulting dataset:"
+	print outputdata	
+ 	if argsdict['output_csv']:
+ 		print header, "output_csv is activated."
+ 		print "Please choose a directory and file name to save the csv file to."
+ 		root=Tkinter.Tk()
+		root.withdraw()
+		root.update()
+		output_csv = tkFileDialog.asksaveasfile(mode='w', defaultextension=".csv")
+		#output_csv=tkFileDialog.askopenfilename(title="Please choose file name for csv output.")
+		outputdata.to_csv(output_csv, encoding="utf-8", index=False)
+		print header, "Written csv spreadsheet to {}".format(output_csv)
+	if argsdict['move_data']:
+		print header, "move_data is activated."
+		extracter(outputdata, 'gilbert_number')
 	
 	
  
-main('/Users/ps22344/Downloads/tgdp-master/summer16/files/gilbert_questions_withtrans_final.csv', 'transcription', 'deːɐ̯', '/Users/ps22344/Downloads/tgdp-master/summer16/files/testsentencefinder.csv')
+#main('/Users/ps22344/Downloads/tgdp-master/summer16/files/gilbert_questions_withtrans_final.csv', 'transcription', 'deːɐ̯', '/Users/ps22344/Downloads/tgdp-master/summer16/files/testsentencefinder.csv')
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
