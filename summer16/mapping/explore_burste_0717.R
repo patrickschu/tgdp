76e2dfcb1f4d6d4bb8e3e565a32d2776f4351748
patrick <ps22344@par102-894475m.local>
2016-07-21 16:56:21 -0500

    add plotting

--- summer16/mapping/explore_burste_0717.R
+++ summer16/mapping/explore_burste_0717.R
@@ -1,7 +1,7 @@
-#exploratoty plotting after first measurements of hairbrush
+#exploratory plotting after first measurements of hairbrush
 
-hair=read.csv('/Users/ps22344/Downloads/tgdp-master/summer16/gilberttools/aseftgregtergtertret.csv', header=T)
-
+hair=read.csv('/Users/ps22344/Downloads/tgdp/summer16/gilberttools/gilbertsound_16.csv', header=T)
+outputfile='sentence16_F1_F2.png'
 cat ("Range F1");
 range(hair$oF1, na.rm=TRUE);
 
@@ -12,14 +12,14 @@ range(hair$oF2, na.rm=TRUE);
 
 
 summary(hair);
-png('buerste_F1_F2.png',  width=960, height=640, res=100);
+png(outputfile,  width=960, height=640, res=100);
 
 plot(hair$oF2, hair$oF1, ylim=c(810, 370), xlim=c(2300,1300), 
-type="n", main="Scatterplot of all tokens of /ue/ in buerste");
+type="n", main="Scatterplot of all tokens of /ue/ in sentence 16");
 
 text( hair$oF2, hair$oF1, labels=hair$speaker_number, col=c("red", "blue")[hair$gender]);
 
-points (1387, 400, col="red", lty=2, cex=20);
+points (1387, 400, col="red", pch=3, cex=10);
 legend('bottomright', legend="Speaker IDs; men in blue, women in red; blue cross is mean from Rausch 72");
 
 dev.off()
