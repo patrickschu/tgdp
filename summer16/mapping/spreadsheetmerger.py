00692dcfcc23015aae9bb7b6c12c76f6a4f1e946
patrick <ps22344@par102-894475m.local>
2016-07-22 14:04:44 -0500

    lots of csv activities

--- summer16/mapping/spreadsheetmerger.py
+++ summer16/mapping/spreadsheetmerger.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
-
+import pandas
+import os
 
 def spreadmerger(spreadsheet_1, spreadsheet_2, filename=False):
 	"""
 	The spreadmerger merges a number of spreadsheets into a giant spreadsheet and outputs it as a .csv. 
 	The outputspreadsheet's name is a merged version of the inputspreadsheet with the _merged added.
+	Watch the axis setting.
 	"""
 	spreadsheet1=pandas.read_csv(spreadsheet_1, encoding="utf-8")
 	spreadsheet2=pandas.read_csv(spreadsheet_2, encoding="utf-8")
-	outputspread=pandas.concat([spreadsheet1,spreadsheet2], axis=1)
-	print os.path.split(spreadsheet_1)
-	outputspread.to_csv(os.path.join(otadir, "outputfiles", os.path.split(spreadsheet_1)[1]+os.path.split(spreadsheet_2)[1]+"_merged.csv"), encoding="utf-8")
+	outputspread=pandas.concat([spreadsheet1,spreadsheet2])
+	outputspread.to_csv(os.path.join(os.path.split(spreadsheet_1)[1]+os.path.split(spreadsheet_2)[1]+"_merged.csv"), encoding="utf-8", na_rep="NA", index=False, cols=spreadsheet2.columns)
+	print "File written to", os.path.join(os.path.split(spreadsheet_1)[1]+os.path.split(spreadsheet_2)[1]+"_merged.csv")
 	return outputspread
 
-spreadmerger('/Users/ps22344/Downloads/ota/outputfiles/hapaxes_25yearperiods_overall_13_50_07_21.csv', '/Users/ps22344/Downloads/ota/outputfiles/hapaxes_25yearperiods_ment_13_39_07_21.csv' )
+spreadmerger('gilbertsound_1.csvgilbertsound_16.csv_merged.csvgilbertsound_21.csv_merged.csvgilbertsound_111.csv_merged.csv', '/Users/ps22344/Downloads/tgdp/summer16/gilberttools/gilbertsound_125.csv' )
