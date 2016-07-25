b37ac20aaacdca91e5847a22e952a804b7129045
patrick <ps22344@par102-894475m.local>
2016-07-25 16:43:54 -0500

    fix issues with NAs in spreadsheetmerger

--- summer16/mapping/spreadsheetmerger.py
+++ summer16/mapping/spreadsheetmerger.py
@@ -13,8 +13,10 @@ def spreadmerger(spreadsheet_1, spreadsheet_2, filename=False):
 	spreadsheet2=pandas.read_csv(spreadsheet_2, encoding="utf-8")
 	###THIS IS NOT RIGHT, INTRODUCES Nan --> no, the nas are produced by the dataadder
 	outputspread=pandas.concat([spreadsheet1,spreadsheet2])
-	outputspread.to_csv(os.path.join(os.path.split(spreadsheet_1)[1]+os.path.split(spreadsheet_2)[1]+"_merged.csv"), encoding="utf-8", na_rep="NA", index=False, cols=spreadsheet2.columns)
+	#check how long a file name may be, construct emergency exit
+	#outputspread.to_csv(os.path.join(os.path.split(spreadsheet_1)[1]+os.path.split(spreadsheet_2)[1]+"_merged.csv"), encoding="utf-8", na_rep="NA", index=False, cols=spreadsheet2.columns)
+	outputspread.to_csv("shortishortue_merged.csv", encoding="utf-8", na_rep="NA", index=False, cols=spreadsheet2.columns)
 	print "File written to", os.path.join(os.path.split(spreadsheet_1)[1]+os.path.split(spreadsheet_2)[1]+"_merged.csv")
 	return outputspread
 
-spreadmerger('/Users/ps22344/Downloads/tgdp/summer16/gilberttools/gilbertsound_74_shorti.csv', '/Users/ps22344/Downloads/tgdp/summer16/gilberttools/gilbertsound_125_shorti.csv' )
+spreadmerger('/Users/ps22344/Downloads/tgdp/summer16/mapping/gilbertsound_1.csvgilbertsound_16.csv_merged.csvgilbertsound_21.csv_merged.csvgilbertsound_111.csv_merged.csvgilbertsound_125.csv_merged.csv', '/Users/ps22344/Downloads/tgdp/summer16/mapping/gilbertsound_26_shorti.csvgilbertsound_74_shorti.csv_merged.csvgilbertsound_114_shorti.csv_merged.csvgilbertsound_125_shorti.csv_merged.csv' )
