5424c3b541e35b5eaaeae2e9a1c4bff99b70ece6
patrick <ps22344@par102-894475m.local>
2016-07-25 12:54:41 -0500

    test the NA setting in dataadder

--- summer16/gilberttools/dataadder_0717.py
+++ summer16/gilberttools/dataadder_0717.py
@@ -51,9 +51,9 @@ def dataadder(input_spread, file_name_column, input_folder, cat_to_add="new_colu
 			except UnicodeError, err:
 				print "\nUNICODE ISSUE -- FILE {} WAS NOT PROCESSED; probably need to re-create in Praat\n".format(fili), err			
 	print header, inputspread
-	with open("gilbertsound_74_shorti.csv", "w") as outputspread:
+	with open("gilbertsound_114_shorti.csv", "w") as outputspread:
 		inputspread.to_csv(outputspread, encoding='utf-8', index=False)
 	print header, "File written to", outputspread
 	
 	
-dataadder('/Users/ps22344/Desktop/dataset_rounding/short_i/74/74.csv', 'id', "/Users/ps22344/Desktop/dataset_rounding/short_i/74/74_measurements", 'F1')
+dataadder('/Users/ps22344/Desktop/dataset_rounding/short_i/114/114.csv', 'id', "/Users/ps22344/Desktop/dataset_rounding/short_i/114/114_measurements", 'F1')
