41bed59cbc6e1ddbb697082e8ba244c3adf3edfe
U-laptop\ps <patrick@email.com>
2016-08-12 23:54:42 -0500

    add merging

--- summer16/mapping/spreadsheetmerger.py
+++ summer16/mapping/spreadsheetmerger.py
@@ -15,8 +15,8 @@ def spreadmerger(spreadsheet_1, spreadsheet_2, filename=False):
 	outputspread=pandas.concat([spreadsheet1,spreadsheet2])
 	#check how long a file name may be, construct emergency exit
 	#outputspread.to_csv(os.path.join(os.path.split(spreadsheet_1)[1]+os.path.split(spreadsheet_2)[1]+"_merged.csv"), encoding="utf-8", na_rep="NA", index=False, cols=spreadsheet2.columns)
-	outputspread.to_csv("shortishortue_merged.csv", encoding="utf-8", na_rep="NA", index=False, cols=spreadsheet2.columns)
+	outputspread.to_csv(os.path.join(os.path.split(spreadsheet_1)[1]+os.path.split(spreadsheet_2)[1]+"_merged.csv"), encoding="utf-8", na_rep="NA", index=False, cols=spreadsheet2.columns)
 	print "File written to", os.path.join(os.path.split(spreadsheet_1)[1]+os.path.split(spreadsheet_2)[1]+"_merged.csv")
 	return outputspread
 
-spreadmerger('/Users/ps22344/Downloads/tgdp/summer16/mapping/gilbertsound_1.csvgilbertsound_16.csv_merged.csvgilbertsound_21.csv_merged.csvgilbertsound_111.csv_merged.csvgilbertsound_125.csv_merged.csv', '/Users/ps22344/Downloads/tgdp/summer16/mapping/gilbertsound_26_shorti.csvgilbertsound_74_shorti.csv_merged.csvgilbertsound_114_shorti.csv_merged.csvgilbertsound_125_shorti.csv_merged.csv' )
+spreadmerger('/home/ps/tgdp/summer16/mapping/gilbertsound_1.csvgilbertsound_16.csv_merged.csvgilbertsound_21.csv_merged.csvgilbertsound_111.csv_merged.csvgilbertsound_125.csv_merged.csv', '/home/ps/tgdp/summer16/mapping/shorti_final_FIXED.csv')
